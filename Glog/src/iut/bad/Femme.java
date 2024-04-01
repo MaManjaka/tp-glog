3aa01faa028c9538732d88892cc35ef7058e499c
MiMarika <MiMarika@DESKTOP-68LSFSI>
2024-04-01 14:01:30 +0200

    question13

--- Glog/src/iut/bad/Femme.java
+++ Glog/src/iut/bad/Femme.java
@@ -7,4 +7,9 @@ public class Femme extends Humain{
 		this.prenom=prenom;
 		this.age=age;
 	}
+	public static void main(String[] args) {
+	    Homme homme = new Homme("Jean", "Dupont", 30);
+	    Femme femme = new Femme("Marie", "Dupont", 25);
+	    femme.ami(homme);
+	}
 }
