38dc127d11f967b11fcf079fc333bf350bef0ee7
MiMarika <MiMarika@DESKTOP-68LSFSI>
2024-04-01 14:57:51 +0200

    question1refactoring

---
+++ Glog/src/fr/emse/test/MoneyTest.java
@@ -0,0 +1,27 @@
+package fr.emse.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+public class MoneyTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	
+	
+	@Test
+	public void testSimpleAdd() {
+	Money m12CHF = new Money(12, "CHF"); // création de données
+	Money m14CHF = new Money(14, "CHF");
+	Money expected = new Money(26, "CHF");
+	Money result = m12CHF.add(m14CHF); // exécution de la méthode testée assertTrue(expected.equals(result)); // comparaison
+	}
+}
