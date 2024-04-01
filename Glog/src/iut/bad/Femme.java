3c22951b8f5caf1d2359c4b99522f8e1ee9cfd5b
MiMarika <MiMarika@DESKTOP-68LSFSI>
2024-04-01 11:05:41 +0200

    question6

--- Glog/src/iut/bad/Femme.java
+++ Glog/src/iut/bad/Femme.java
@@ -2,7 +2,7 @@ package iut.bad;
 
 public class Femme extends Humain{
 	
-	public Femme(String nom, String prenom, int age) {
+	public Femme(String nom, String prenom, int age){
 		this.nom= nom;
 		this.prenom=prenom;
 		this.age=age;
