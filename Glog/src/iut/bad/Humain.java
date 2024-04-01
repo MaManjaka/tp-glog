9c46a82cf44343a175174cd321fe0780cd00c83c
MiMarika <MiMarika@DESKTOP-68LSFSI>
2024-04-01 11:15:17 +0200

    question7

--- Glog/src/iut/bad/Humain.java
+++ Glog/src/iut/bad/Humain.java
@@ -4,4 +4,9 @@ public abstract class Humain {
 	protected String nom;
 	protected String prenom;
 	protected int age;
+	
+	protected void details() {
+		System.out.println("Nom:"+nom+ " \nPrenom:"+prenom+ " \nAge:"+age);
+	}
+	
 }
