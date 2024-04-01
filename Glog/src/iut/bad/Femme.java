1772f572ac7d6e21675fc3d07b21db2b04883f28
MiMarika <MiMarika@DESKTOP-68LSFSI>
2024-04-01 10:46:38 +0200

    question4

---
+++ Glog/src/iut/bad/Femme.java
@@ -0,0 +1,13 @@
+package iut.bad;
+
+public class Femme {
+	String nom;
+	String prenom;
+	int age;
+	
+	public Femme(String nom, String prenom, int age) {
+		this.nom= nom;
+		this.prenom=prenom;
+		this.age=age;
+	}
+}
